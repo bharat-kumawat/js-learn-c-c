bd20f915ed50745b27297ac8ddd7af3b451a96a1
bharat-kumawat <141985924+bharat-kumawat@users.noreply.github.com>
2024-08-26 04:30:13 +0000

    conversion of datatype

---
+++ 01_basics/03_datatype _conversion.js
@@ -0,0 +1,24 @@
+let score = null
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber)
+// "33" => 33
+// "33Bac" => NAN
+//true => 1
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1=> true ; 0 => false
+// "" => false
+//"bhaarat" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
