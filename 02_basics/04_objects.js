5834d1e7e6f8e22e1b80dfdbae5c8135332fe1a9
bharat-kumawat <141985924+bharat-kumawat@users.noreply.github.com>
2024-08-26 15:27:07 +0000

    object foundation

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,53 @@
+// const tinderUSe = new Object()
+const tinderUSe = {}
+tinderUSe.id = "123453"
+tinderUSe.name = "sammay"
+tinderUSe.isLoggedIn = false
+
+// console.log(tinderUSe)
+const regularUser = {
+    email: "some@gmail.com",
+    fullname:{
+    userFullname: {
+        firstname: "bharat",
+        lastname: "kumawat"
+    }
+    }
+}
+  
+// console.log(regularUser.fullname.userFullname)
+
+const obj1 = {1: "a",2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5:"a", 6: "b"}
+
+//const obj3 = {obj1, obj2}
+//const obj3= Object.assign({}, obj1,obj2, obj4)
+
+const obj3 = {...obj1, ...obj2,...obj4}
+console.log(obj3)
+
+const user= [
+    {
+
+    },
+{
+
+},
+{
+
+},
+{
+
+},{
+
+},
+]
+user[1].email
+//console.log(tinderUSe)
+
+console.log(Object.keys(tinderUSe))
+console.log(Object.values(tinderUSe))
+console.log(Object.entries(tinderUSe))
+
+console.log(tinderUSe.hasOwnProperty('isLoggedIn'))
