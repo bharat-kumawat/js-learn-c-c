92f22ff1b5a78071ab7548c90ecf2d31e9430c2a
bharat-kumawat <141985924+bharat-kumawat@users.noreply.github.com>
2024-08-26 04:13:23 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accounId = 12345
+let accountEmail = "bharat@gmail.com"
+var  accountPassword = "123216"
+accountCity = "Pratapgarh"
+
+//accounId = 2   not allowed
+accountEmail = "given@gmail.com"
+accountPassword = 4848499
+console.table([accountPassword,accounId,accountEmail,accountCity])
+
+/* not use var because of issue in block scope and functional scope*/
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3)  // we are using node js not browser
+
+// console.log(3+
+//     3) //code readability is neccessary
+
+let name = "bharat"
+let age = 30
+let isLoggedIn = false
+
+//bigint
+//string
+//boolean true/false
+//null  =>standalone value
+//undefined => value declare but not assign
+// symbol //for find the unique 
+//object
+
+console.log(typeof(name));
