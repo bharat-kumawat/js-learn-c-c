f11c6a736f51c6eeb73b3688a2fee9e8f2aff525
bharat-kumawat <141985924+bharat-kumawat@users.noreply.github.com>
2024-08-27 18:25:33 +0000

    foreach concept

---
+++ 05_loops_Iteration/FOREACH.js
@@ -0,0 +1,32 @@
+const coding =["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+    
+// })
+
+const myCoding = [
+    {   languageName: "javascript",
+        languageFileName: "js"
+    },
+    {   languageName: "java",
+        languageFileName: "java"
+    },
+    {   languageName: "Python",
+        languageFileName: "py"
+    }
+];
+
+myCoding.forEach( (item)=> {
+    console.log(item.languageName);
+    
+})
